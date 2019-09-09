adb307a414bf12e2cb17e853ab2481dd034d46cf
heptacode <admin@hyunwoo.kim>
2019-09-10 01:05:37 +0900

    Redesign App Bar

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,19 @@
 <template>
   <v-app class="overflow-hidden">
-    <v-app-bar
-      absolute
-      color="#69cca2"
-      dark
-      shrink-on-scroll
-      prominent
-      src="https://picsum.photos/1920/1080?random"
-      fade-img-on-scroll
-      scroll-target="#scroll-target"
-      scroll-threshold="500"
-    >
-      <template v-slot:img="{ props }">
-        <v-img
-          v-bind="props"
-          gradient="to top right, rgba(55,236,186,.7), rgba(25,32,72,.7)"
-        ></v-img>
-      </template>
-
+    <v-app-bar app fixed color="#69cca2" dark>
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-toolbar-title>SafeForest</v-toolbar-title>
+      <v-toolbar-title><strong>SafeForest</strong></v-toolbar-title>
 
       <div class="flex-grow-1"></div>
-      <div id="nav">
-        <router-link to="/">home</router-link>
-        <router-link to="/about">about</router-link>
-      </div>
+
+      <nav>
+        <router-link to="/">홈</router-link>
+        <router-link to="/about">소개</router-link>
+      </nav>
     </v-app-bar>
-    <v-sheet id="scroll-target" class="overflow-y-auto" height="100vh">
-      <router-view />
-    </v-sheet>
+
+    <router-view />
   </v-app>
 </template>
 
@@ -38,16 +21,34 @@
 html {
   overflow: hidden;
 }
-#nav {
+
+html::-webkit-scrollbar {
+  display: none;
+}
+
+nav {
   padding: 30px;
 }
 
-#nav a {
+nav a {
+  text-decoration: none;
   font-weight: bold;
+  font-size: 22px;
   color: #2c3e50;
+  margin: 0 1vw;
+  transition-duration: 200ms;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+nav a:link,
+nav a:visited {
+  color: white;
+}
+nav a:hover {
+  text-shadow: 0 5px 3px rgba(0, 0, 0, 0.2);
+}
+nav a.router-link-exact-active {
+  padding-bottom: 3px;
+  border-bottom: 2px solid white;
+  text-shadow: 0 5px 3px rgba(0, 0, 0, 0.2);
 }
 </style>
